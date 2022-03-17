a45912d37a12bcc0687a3ed08e5577cd2b15b8c6
rcannood <rcannood@users.noreply.github.com>
2022-03-17 11:19:43 +0000

    deploy: aaab58e27188ac5b8e0767197b3c3718447634b6

--- src/download/sync_resources_test/run_test.sh
+++ src/download/sync_resources_test/run_test.sh
@@ -6,7 +6,7 @@
   --dryrun > \
   output.txt
 
-if ! grep -q '(dryrun) download: s3://czbiohub.*to.*fastq.gz' output.txt; then
+if ! grep -q '(dryrun) download: s3://czbiohub.*to.*raw' output.txt; then
   echo Could not find content
   exit 1
 fi
