f46224ae4f7a4f31b0f9a8bbaf63dd78d3272825
rcannood <rcannood@users.noreply.github.com>
2022-03-22 06:14:14 +0000

    deploy: b599d081c1f50b178276cadcc1039563156f496c

--- src/convert/thermo_to_mzml/script.sh
+++ src/convert/thermo_to_mzml/script.sh
@@ -5,4 +5,20 @@ par_input="resources_test/zenodo_4274987/raw/Sample1.raw"
 par_output="output.hzML"
 ## VIASH END
 
-mono /var/local/thermorawfileparser/ThermoRawFileParser.exe "-i=$par_input" "-b=$par_output"
+
+# need to copy file because tool doesn't work with symlinks
+
+# create tempdir
+MY_TEMP="${VIASH_TEMP:-/tmp}"
+TMPDIR=$(mktemp -d "$MY_TEMP/$meta_functionality_name-XXXXXX")
+function clean_up {
+  [[ -d "$TMPDIR" ]] && rm -r "$TMPDIR"
+}
+trap clean_up EXIT
+
+tmp_input="$TMPDIR/`basename "$par_input"`"
+cp "$par_input" "$tmp_input"
+
+mono /var/local/thermorawfileparser/ThermoRawFileParser.exe "-i=$tmp_input" "-b=$par_output"
+
+# mono /var/local/thermorawfileparser/ThermoRawFileParser.exe "-i=$par_input" "-b=$par_output"
