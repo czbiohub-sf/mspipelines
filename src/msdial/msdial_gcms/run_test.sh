7d6e989ab05dfefd182038de2875006eed487283
rcannood <rcannood@users.noreply.github.com>
2022-07-12 12:49:13 +0000

    deploy: 215a2a3965cdb1384be3483e53c589cabc63b4d5

--- src/msdial/msdial_gcms/run_test.sh
+++ src/msdial/msdial_gcms/run_test.sh
@@ -1,33 +1,12 @@
 #!/bin/bash
 
-# testing whole directories
-echo ">> Running MSDial (dir mode)"
-./$meta_functionality_name \
-  --input "$resources_dir/GCMS" \
-  --output "output1" \
-  --ri_index_file "$resources_dir/GCMS/FAMEs RT.txt" | \
-  tee output1.txt
-
-echo ">>> Checking contents of stdout"
-if ! grep -q 'All processing finished' output1.txt; then
-  echo Could not find content
-  exit 1
-fi
-
-echo ">>> Checking whether output files can be found"
-if ! ls output1/140428actsa25_* > /dev/null 2>&1; then echo "Output file 25 could not be found!"; exit 1; fi
-if ! ls output1/140428actsa26_* > /dev/null 2>&1; then echo "Output file 26 could not be found!"; exit 1; fi
-if ! ls output1/140428actsa27_* > /dev/null 2>&1; then echo "Output file 27 could not be found!"; exit 1; fi
-if ! ls output1/AlignResult-* > /dev/null 2>&1; then echo "Output file alignresult could not be found!"; exit 1; fi
-[[ ! -f output1/params.txt ]] && echo "Param file could not be found!" && exit 1
-[[ ! -f output1/ri_index_paths.txt ]] && echo "RI Index file could not be found!" && exit 1
-
-
 # testing individual files
-echo ">> Running MSDial (file mode)"
-./$meta_functionality_name \
+echo ">> Running MSDial"
+$meta_executable \
   --input "$resources_dir/GCMS/140428actsa25_1.cdf" \
   --input "$resources_dir/GCMS/140428actsa27_1.cdf" \
+  --class_id foo \
+  --class_id bar \
   --output "output2" | \
   tee output2.txt
 
