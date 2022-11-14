4956b5d5f3bf9a29d3da3d048c873d6680db8ae1
Kenneth-DI <kenneth@data-intuitive.com>
2022-11-14 10:37:16 +0100

    Update maxquant_demo_files.sh script
    - Change ID
    - Change indentation (single tab)
    - Use seqkit to extract the first 1000 entries

--- workflows/resources_test_scripts/maxquant_demo_files.sh
+++ workflows/resources_test_scripts/maxquant_demo_files.sh
@@ -7,7 +7,7 @@ REPO_ROOT=$(git rev-parse --show-toplevel)
 cd "$REPO_ROOT"
 
 # Settings
-ID="maxquant_11112022"
+ID="maxquant_demo_files"
 OUT="resources_test/$ID"
 DIR="$OUT"
 
@@ -22,35 +22,30 @@ trap clean_up EXIT
 # download bcl data
 raw_dir="$OUT/raw"
 if [ ! -f "$raw_dir/Sample.raw" ]; then
+  echo "Preparing sample RAW file..."
   mkdir -p "$raw_dir"
-  tmp_fasta="$TMPDIR/tmp.fasta"
-
   wget https://ftp.pride.ebi.ac.uk/pride/data/archive/2014/04/PXD000561/Fetal_Testis_bRP_Elite_26_f20.raw -O "$raw_dir/Sample.raw"
+fi
+
+if [ ! -f "$raw_dir/reference.fasta" ] ; then
+  echo "Preparing sample FASTA file..."
+  tmp_fasta="$TMPDIR/tmp.fasta"
   wget https://zenodo.org/record/4274987/files/Protein_database.fasta -O "$tmp_fasta"
 
-# process the fasta (subsample first 1000) entries
- if [ -f "$raw_dir/reference.fasta" ] ; then
-    rm "$raw_dir/reference.fasta"
- fi
-
-i=0
-while read -r line; do
-if [[ "$line" =~ ^\>.*  ]]; then
-    let i++
-fi
-if (($i < 1000)) then
-    echo $line >> "$raw_dir/reference.fasta"
-fi
-done <$tmp_fasta 
-rm $tmp_fasta
-
-fi
-
-maxquant_out="$OUT/maxquant_out"
-if [ ! -f "$maxquant_out" ]; then
-  mkdir -p "$maxquant_out" 
-  target/docker/maxquant/maxquant/maxquant \
-    --input "$raw_dir/Sample.raw" \
-    --reference "$raw_dir/reference.fasta" \
-    --output "$maxquant_out"
+  if ! command -v seqkit &> /dev/null; then
+    echo "This script requires seqkit. Please make sure the binary is added to your PATH."
+  # process the fasta (subsample first 1000) entries TODO keep this as a plan B strat in case the binaries are missing?
+  # i=0
+  # while read -r line; do
+  #   if [[ "$line" =~ ^\>.*  ]]; then
+  #     let i++
+  #   fi
+  #   if (($i < 1000)); then
+  #     echo $line >> "$raw_dir/reference.fasta"
+  #   fi
+  # done <$tmp_fasta 
+    exit 1
+  fi
+  seqkit head -n 1000 $tmp_fasta -o "$raw_dir/reference.fasta"
+  rm $tmp_fasta
 fi
