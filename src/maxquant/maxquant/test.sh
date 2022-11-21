38f033d4416a35bac4de03872bd783a5fa37ed57
Dries Schaumont <5946712+DriesSchaumont@users.noreply.github.com>
2022-11-21 13:35:13 +0100

    Update test.sh

--- src/maxquant/maxquant/test.sh
+++ src/maxquant/maxquant/test.sh
@@ -1,15 +1,18 @@
-#!/bin/bash
-
 set -eo pipefail
 
+echo ">> Running $meta_functionality_name"
 "$meta_executable" \
-    --input "$meta_resources_dir/zenodo_4274987/raw/Sample1.raw" \
-    --reference "$meta_resources_dir/zenodo_4274987/raw/reference.fasta" \
+    --input "$meta_resources_dir/maxquant_demo_files/raw/Sample.raw" \
+    --reference "$meta_resources_dir/maxquant_demo_files/raw/reference.fasta" \
     --ref_taxonomy_id "9606" \
     --match_between_runs "true" \
     --ms_instrument "Orbitrap" \
     --lcms_run_type "Standard" \
     --lfq_mode "LFQ" \
-    --publishDir "test/"
+    --output "output"
 
-echo ">> Run succeeded"
+echo ">> Checking whether output files can be found"
+[[ ! -f "output/mqpar.xml" ]] && echo "Output mqpar.xml does not exist" && exit 1
+[[ ! -f "output/combined/txt/proteinGroups.txt" ]] && echo "Output proteinGroups.txt does not exist" && exit 1
+
+echo ">> All tests succeeded!"
